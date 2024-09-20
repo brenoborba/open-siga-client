065950b384a8b4100f5c8ad5bf7e3a5b8931ab3e
Breno Bessa Borba Alcantara <a40541@alunos.ipb.pt>
2024-09-20 20:27:29 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button, Typography } from "@material-tailwind/react"
+
+export default function App() {
+  return (
+    <div>
+      <Typography placeholder="" variant="h1">
+        OpenSiga
+      </Typography>
+      <Button placeholder="">Button</Button>
+    </div>
+  )
+}
