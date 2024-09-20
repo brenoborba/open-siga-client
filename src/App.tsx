a557a51dbd7e4371ce806d66e5771d3c4ba3da21
Breno Bessa Borba Alcantara <a40541@alunos.ipb.pt>
2024-09-20 22:08:04 +0100

    feat(ui): add signin page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
-import { Button, Typography } from "@material-tailwind/react"
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  Input,
+  Typography,
+} from "@material-tailwind/react"
 
 export default function App() {
   return (
-    <div>
-      <Typography placeholder="" variant="h1">
-        OpenSiga
-      </Typography>
-      <Button placeholder="">Button</Button>
+    <div className="flex h-screen flex-col items-center justify-center">
+      <Typography variant="h1">OpenSiga</Typography>
+
+      <Card className="w-96">
+        <CardBody>
+          <Typography variant="lead" className="mb-4">
+            Sign in
+          </Typography>
+          <div className="mb-2 space-y-4">
+            <Input placeholder="Email" label="Email" size="lg" type="email" />
+            <Input
+              placeholder="Password"
+              label="Password"
+              size="lg"
+              type="password"
+            />
+          </div>
+
+          <Typography variant="paragraph" className="text-sm">
+            Forgot your password? <a href="#">Reset it here</a>
+          </Typography>
+        </CardBody>
+        <CardFooter className="flex pt-0">
+          <Button color="black" size="lg" className="ml-auto">
+            Sign in
+          </Button>
+        </CardFooter>
+      </Card>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import "./index.css"
 
-import { ThemeProvider } from "@material-tailwind/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { ThemeProvider } from "@material-tailwind/react"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <RouterProvider router={router} />
     <ThemeProvider>
       <App />
     </ThemeProvider>
   </React.StrictMode>,
-);
+)
